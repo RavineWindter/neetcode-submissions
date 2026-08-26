c261e39e6503bc9ea54b310a597c0eb4e4fb77d7
RavineWindter <83411206+RavineWindter@users.noreply.github.com>
2026-08-26 15:24:52 +0100

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.cpp
@@ -0,0 +1,40 @@
+#include <ranges>
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        constexpr int row_offset = 9*0;
+        constexpr int collumn_offset = 9*1;
+        constexpr int sub_box_offset = 9*2;
+        
+        std::vector<std::uint32_t> checks(3*9, 0);
+        for (const int i : std::views::iota(0, 9))
+        {
+            for (const int j : std::views::iota(0, 9))
+            {
+                const auto value = board[i][j];
+                if (value < '1' || value > '9')
+                {
+                    continue;
+                }
+                const std::uint32_t bit_mask = 1U << (board[i][j] - '0');
+                const auto row = row_offset + i;
+                const auto collumn = collumn_offset + j;
+                const auto sub_box = sub_box_offset + (j/3)+(3*(i/3));
+
+                if ((checks[row] & bit_mask) ||
+                    (checks[collumn] & bit_mask) ||
+                    (checks[sub_box] & bit_mask))
+                {
+                    return false;
+                }
+
+                checks[row] |= bit_mask;
+                checks[collumn] |= bit_mask;
+                checks[sub_box] |= bit_mask;
+            }
+        }
+
+        return true;
+    }
+};
