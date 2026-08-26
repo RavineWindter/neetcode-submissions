b6c4c28cebd1f5d5002111788181089fa1442f34
RavineWindter <83411206+RavineWindter@users.noreply.github.com>
2026-08-26 17:24:45 +0100

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start < end)
+        {
+            while (!std::isalnum(s[start]))
+            {
+                ++start;
+            }
+            while (!std::isalnum(s[end]))
+            {
+                --end;
+            }
+            if (start < end && std::toupper(s[start]) != std::toupper(s[end]))
+            {
+                return false;
+            }
+            ++start;
+            --end;
+        }
+        return true;
+    }
+};
